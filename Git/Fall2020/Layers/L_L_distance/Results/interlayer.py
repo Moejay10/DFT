a8bae111745651764e098f22ad80ba4666f6fb63
Mohamed Ismail <mohe95@hotmail.no>
2020-10-07 12:07:07 +0200

    Modified file

---
+++ Git/Fall2020/Layers/L_L_distance/Results/interlayer.py
@@ -0,0 +1,73 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+filename = ["Run2/DFT_D3.txt", "Run2/LDA.txt", "Run2/PBE.txt", "Run2/rev_vdW_DF2.txt", "Run2/vdW_opt88.txt"]
+
+for files in filename:
+    file = open(files, "r")
+    lines_read = file.readlines()
+    N = len(lines_read)
+
+    etot = []
+
+    for i in range(1,N):
+        line = lines_read[i]
+        pieces = line.split()
+        a = float(pieces[2])
+        etot.append(a)
+
+
+    temp = etot[1]
+    etot.pop(1)
+    etot.append(temp)
+    if files == filename[0]:
+        etot_0 = np.array(etot)
+
+    elif files == filename[1]:
+        etot_1 = np.array(etot)
+
+    elif files == filename[2]:
+        etot_2 = np.array(etot)
+
+    elif files == filename[3]:
+        etot_3 = np.array(etot)
+
+    else:
+        etot_4 = np.array(etot)
+
+
+
+x = np.linspace(0.0, 5.0, 11)
+n = 24
+
+plt.plot(x, etot_0/n, "-or")
+plt.plot(x, etot_1/n, "-ob")
+plt.plot(x, etot_2/n, "-og")
+plt.plot(x, etot_3/n, "-oy")
+plt.plot(x, etot_4/n, "-o")
+plt.legend(["DFT_D3.txt", "LDA.txt", "PBE.txt", "rev_vdw.txt", "vdw_opt88.txt"])
+plt.title("All functionals")
+plt.xlabel("Interlayer space [Å]")
+plt.ylabel("Energy/atom [meV]")
+plt.show()
+
+plt.plot(x, etot_0/n, "-or")
+plt.plot(x, etot_2/n, "-og")
+plt.plot(x, etot_3/n, "-oy")
+plt.plot(x, etot_4/n, "-o")
+plt.legend(["DFT_D3.txt", "PBE.txt", "rev_vdw.txt", "vdw_opt88.txt"])
+plt.title("All functionals except LDA")
+plt.xlabel("Interlayer space [Å]")
+plt.ylabel("Energy/atom [meV]")
+plt.show()
+
+
+plt.plot(x, etot_1/n, "-ob")
+plt.legend(["LDA.txt"])
+plt.title("LDA functional")
+plt.xlabel("Interlayer space [Å]")
+plt.ylabel("Energy/atom [meV]")
+plt.show()
