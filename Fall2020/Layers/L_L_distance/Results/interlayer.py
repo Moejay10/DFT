c207457749a79d9ecf33b6c22a52e24ecc3c85ed
Mohamed Ismail <mohe95@hotmail.no>
2020-10-09 11:46:36 +0200

    Modified interlayer.py

--- Fall2020/Layers/L_L_distance/Results/interlayer.py
+++ Fall2020/Layers/L_L_distance/Results/interlayer.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-filename = ["DFT_D3.txt", "LDA.txt", "PBE.txt", "rev_vdw.txt", "vdw_opt88.txt"]
+
+filename = ["Run2/DFT_D3.txt", "Run2/LDA.txt", "Run2/PBE.txt", "Run2/rev_vdW_DF2.txt", "Run2/vdW_opt88.txt"]
 
 for files in filename:
     file = open(files, "r")
@@ -18,6 +19,10 @@ for files in filename:
         a = float(pieces[2])
         etot.append(a)
 
+
+    temp = etot[1]
+    etot.pop(1)
+    etot.append(temp)
     if files == filename[0]:
         etot_0 = np.array(etot)
 
@@ -33,17 +38,36 @@ for files in filename:
     else:
         etot_4 = np.array(etot)
 
-x = np.linspace(0.05, 0.05*N, N)
-n = 24
-print(x)
 
-plt.plot(x[:-1], etot_0/n, "-or")
-plt.plot(x[:-1], etot_1/n, "-ob")
-plt.plot(x[:-1], etot_2/n, "-og")
-plt.plot(x[:-1], etot_3/n, "-oy")
-plt.plot(x[:-1], etot_4/n, "-o")
+
+x = np.linspace(0.0, 5.0, 11)
+n = 24
+
+plt.plot(x, etot_0/n, "-or")
+plt.plot(x, etot_1/n, "-ob")
+plt.plot(x, etot_2/n, "-og")
+plt.plot(x, etot_3/n, "-oy")
+plt.plot(x, etot_4/n, "-o")
 plt.legend(["DFT_D3.txt", "LDA.txt", "PBE.txt", "rev_vdw.txt", "vdw_opt88.txt"])
-#plt.title("")
+plt.title("All functionals")
+plt.xlabel("Interlayer space [Å]")
+plt.ylabel("Energy/atom [meV]")
+plt.show()
+
+plt.plot(x, etot_0/n, "-or")
+plt.plot(x, etot_2/n, "-og")
+plt.plot(x, etot_3/n, "-oy")
+plt.plot(x, etot_4/n, "-o")
+plt.legend(["DFT_D3.txt", "PBE.txt", "rev_vdw.txt", "vdw_opt88.txt"])
+plt.title("All functionals except LDA")
+plt.xlabel("Interlayer space [Å]")
+plt.ylabel("Energy/atom [meV]")
+plt.show()
+
+
+plt.plot(x, etot_1/n, "-ob")
+plt.legend(["LDA.txt"])
+plt.title("LDA functional")
 plt.xlabel("Interlayer space [Å]")
 plt.ylabel("Energy/atom [meV]")
 plt.show()
